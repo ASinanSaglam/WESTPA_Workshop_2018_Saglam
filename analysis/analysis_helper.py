7e241caf8f8f6e5bbef28fb04f9877670b5c8436
ASinanSaglam <asinansaglam@gmail.com>
2018-07-06 16:44:36 -0400

    Further modifying things to work with Jupyter

---
+++ analysis/analysis_helper.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def row_normalize(mat):
+    rnmat = np.zeros(mat.shape)
+    for irow, row in enumerate(mat):
+        if row.sum() != 0:
+            rnmat[irow] = row/row.sum() 
+    return rnmat
+
+def symmetrize(mat):
+    smat = np.zeros(mat.shape)
+    smat = 0.5*(mat+mat.T)
+    return smat
+
+def get_bin_labels(assign):
+    bin_labels_str = assign['bin_labels'][...]
+    bin_labels = []
+    for ibstr, bstr in enumerate(bin_labels_str):
+        st, ed = bstr.find('['), bstr.find(']')
+        bin_labels.append(eval(bstr[st:ed+1]))
+    bin_labels = np.array(bin_labels[1:])
+    return bin_labels
--- analysis/make_pcca.py
+++ analysis/make_pcca.py
@@ -3,7 +3,7 @@ import pickle, h5py, sys
 import numpy as np
 import networkx as nx
 import pyemma as pe 
-import pcca_helper as pccah
+import analysis_helper as ahelper
 
 if __name__ == "__main__":
     tm_file = sys.argv[1]
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     tm = np.load(tm_file)
     tm = tm[:,:][:,:]
 
-    stm = pccah.symmetrize(tm)
-    rtm = pccah.row_normalize(stm)
+    stm = ahelper.symmetrize(tm)
+    rtm = ahelper.row_normalize(stm)
     
     MSM = pe.msm.MSM(rtm, reversible=True)
     pcca = MSM.pcca(pcca_cluster_count)
@@ -26,12 +26,7 @@ if __name__ == "__main__":
     f.close()
     
     a = h5py.File(assign_file, 'r')
-    bin_labels_str = a['bin_labels'][...]
-    bin_labels = []
-    for ibstr, bstr in enumerate(bin_labels_str):
-        st, ed = bstr.find('['), bstr.find(']')
-        bin_labels.append(eval(bstr[st:ed+1]))
-    bin_labels = np.array(bin_labels[1:])
+    bin_labels = ahelper.get_bin_labels(a)
     bin_labels_01 = np.array(map(lambda x: (x[0], x[1]), bin_labels))
     bin_labels_01 = bin_labels_01[1:]
     
@@ -44,8 +39,6 @@ if __name__ == "__main__":
     print("metastab 3")
     print(bin_labels[metastab_ass.T==3].mean(axis=0))
     
-    print("hi/hi", "lo/hi", "hi/lo", "lo/lo")
-    state_labels = {0: "hi/hi", 1: "lo/hi", 2: "hi/lo", 3: "lo/lo"}
     state_colors = {0: "#FF00FF", 1: "#0000FF", 2: "#FF0000", 3: "#000000"}
     tm = pcca.transition_matrix
     node_sizes = pcca.stationary_probability*1000
